073cd6b233daedf7abf4241672ea0b7df8923ac2
fikriamri <famri@alterra.id>
2019-08-28 15:49:21 +0700

    add travis.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" &&
-ssh-add -k ~/.ssh/id_rsa &&
-cd /var/www/helloworld
-git pull
-
-source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop helloworld
-docker rm helloworld
-docker rmi aprynur/helloworld
-docker run -d --name helloworld -p 5000:5000 aprynur/helloworld:latest
+sudo docker stop backend
+sudo docker rm backend
+sudo docker rmi fikriamri/e-commerce-backend:v.1
+sudo docker run -d -p 5020:5020 --name backend fikriamri/e-commerce-backend:v.1
